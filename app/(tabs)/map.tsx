ff7867f831987e2781d8ea1652e628345b23db96
Joe <joescholtz@hotmail.com>
2026-02-19 13:21:55 -0600

    formatting

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
@@ -7,25 +7,18 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <View style={styles.container}>
-      <Text
-        style={[
-          styles.text,
-          { color: colorScheme === "dark" ? "black" : "white" },
-        ]}
-      >
-        map screen
-      </Text>
-    </View>
+    <Text
+      style={[
+        styles.text,
+        { color: colorScheme === "dark" ? "black" : "white" },
+      ]}
+    >
+      map screen
+    </Text>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   text: {
     fontSize: 18,
   },
