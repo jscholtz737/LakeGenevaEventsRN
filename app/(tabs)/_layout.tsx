089d56164ce33ff286f5031bab3ea4af086c3ff0
Joe <joescholtz@hotmail.com>
2026-02-18 19:46:15 -0600

    tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,50 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { StatusBar } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
+    <React.Fragment>
+      <StatusBar
+        barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+      <Tabs>
+        <Tabs.Screen
+          name="map"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Map",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="map" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="all"
+          options={{
+            headerShown: false,
+            tabBarLabel: "All",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="calendar" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Search",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="search" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </React.Fragment>
   );
 }
---
+++ app/(tabs)/all.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={[
+          styles.text,
+          { color: colorScheme === "dark" ? "black" : "white" },
+        ]}
+      >
+        all screen
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 18,
+  },
+});
